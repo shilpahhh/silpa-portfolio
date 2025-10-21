d28e1b3dc21a87fe867b167aece9bc9c6d420dbb
shilpahhh <silpababu2005@gmail.com>
2025-10-21 19:41:10 +0530

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import "./about_style.css";
+
+function About() {
+  return (
+    <div className="about-container" data-aos="fade-up">
+      <h1 className="section-heading-about">About Me</h1>
+      <p className="about-text">
+        Aspiring <strong>Full Stack Developer</strong> and <strong>Web Development enthusiast</strong> with a strong passion for
+        problem-solving and building user-friendly applications. I am eager to contribute my technical
+        skills and creativity to a dynamic development team while continuously learning and adapting
+        to emerging technologies.
+      </p>
+      <p className="about-text">
+        I have an interest in <strong>web development</strong> and enjoy the process of creating websites.
+        Additionally, I find communicating with clients to understand their needs and deliver tailored
+        solutions very fulfilling.
+      </p>
+    </div>
+  )
+}
+
+export default About
+
