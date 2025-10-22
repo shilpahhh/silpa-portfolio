554ce4b45cb38bb5ca27461fefdce1f46e5fba34
shilpahhh <silpababu2005@gmail.com>
2025-10-22 15:55:42 +0530

    Update Education.js

--- src/Education.js
+++ src/Education.js
@@ -5,15 +5,22 @@ function Education() {
    <div className="edu-container" data-aos="fade-up">
 
     <div id='section-one'>
-      <h1 className="section-heading">Education</h1>
+      <h1 className="section-heading-edu">Education</h1>
       </div>
 
       <div className="edu-details">
+
+        <div id='university'>
         <p><strong>BSc Computer Science</strong></p>
         <p>Graduated in 2025 from Mahatma Gandhi University</p>
+        </div>
+
+        <div id='insti'>
         <br />
         <p><strong>Internship:</strong></p>
         <p>Completed Java Full Stack Development Internship from QSpiders Institute, Kochi</p>
+      </div>
+      
       </div>
     </div>
   )
